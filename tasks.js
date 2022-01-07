d544e184902be314685c9372152ba5df9c2c4986
samuel-wd-kirby <samkirby@outlook.com>
2022-01-08 08:30:09 +1300

    I added tasks and said true to the ones I did

---
+++ tasks.js
@@ -0,0 +1,42 @@
+// Written in 
+
+// Two-truth-and-a-lie
+const twoTruthAndALie = true
+
+// Kata-object-and-arrays
+const kataObjectsAndArrays = true
+
+// Kata-data-structures
+const kataDataStructures = true
+
+// Kata-Strngs-Numbers-Modules
+const kataStringsNumbersModules = false
+
+// Kata-Types-Modules
+const kataTypeModules = true
+
+// Bowling-kata
+const bowlingKata = true
+
+// Tdd-bowling-kata
+const tddBowlingKata = true
+
+// Ascii-art-reader
+const asciiArtReader = true
+const asciiArtReaderTesting = false
+
+// Conways
+const conways = false
+
+// ------------------------------------
+
+// Stretches
+
+// Kata-number-patterns
+const kataNumberPatterns = false
+
+// Object-Array-Practice
+const objectArrayPractice = false
+
+// Exercise-Arrays
+const exerciseArrays = true
