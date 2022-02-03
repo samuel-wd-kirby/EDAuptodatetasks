34748c71fddf8bb20d96f50a08273be052dd8d9f
samuel-wd-kirby <samkirby@outlook.com>
2022-02-03 20:05:48 +1300

    Made Several Task Edits

--- tasks.js
+++ tasks.js
@@ -10,7 +10,7 @@ const kataObjectsAndArrays = true
 const kataDataStructures = true
 
 // Kata-Strngs-Numbers-Modules
-const kataStringsNumbersModules = false
+const kataStringsNumbersModules = true
 
 // Kata-Types-Modules
 const kataTypeModules = true
@@ -23,10 +23,32 @@ const tddBowlingKata = true
 
 // Ascii-art-reader
 const asciiArtReader = true
-const asciiArtReaderTesting = false
 
-// Conways
-const conways = false
+// Express Server
+const expressServer = true
+
+// Serve Side Rendering
+const serverSideRendering = true
+
+// Pupparazzi
+const pupparazzi = true
+
+// Heroku Checklist
+const herokuChecklist = true
+
+// Knex Todo Cli
+const knexTodoCli = true
+
+// Database Diagram
+const dataBaseDiagram = true
+
+// Knex Join Stories
+const knexJoinStories = true
+const knexJoinStoriesStretches = true
+
+// Knex Relationship Stories
+const knexRelationshipStories = true
+const knexRelationshipStoriesStretches = false
 
 // ------------------------------------
 
@@ -36,7 +58,42 @@ const conways = false
 const kataNumberPatterns = false
 
 // Object-Array-Practice
-const objectArrayPractice = false
+const objectArrayPractice = true
+const objectArrayPracticeTesting = true
 
 // Exercise-Arrays
 const exerciseArrays = true
+
+// Conways
+const conways = false
+
+// Meowtown
+const meowtown = false
+
+// ------------------------------------
+
+// Group Projects
+const horoscopeGroupProject = true
+const bobzTunez = true
+
+// Extras
+const computerSetup = false
+
+
+
+// 31/01/2022 Tasks
+
+// Things to do again:
+// World wide routing, charlottes-web-log-api,
+// look more into crud and learn how to better use such things
+// Start off with lconing down worldwide routing and working through that
+
+
+// Monkey dart board theory, Account builder
+
+// Choose your market.
+// Select some specifications for stocks, e.g. minimum or max market cap, max or minimum share price, daily trading volume.
+
+// Then pick the amount of stock you would like, how you would like it to be spread, and starting capital for investing. 
+
+// Then maybe ways to make an account, recommend sites.
